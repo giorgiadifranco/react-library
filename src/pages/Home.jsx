d97a353375297bcca4009ca9557b2a0c39884fd1
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-25 10:29:25 +0100

    add paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ export default function Home(){
         </header>
         <main>
             <h1> Home</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda nulla quisquam optio atque obcaecati ex modi eveniet impedit delectus facere officiis earum natus eum quibusdam veritatis tempora sed enim in cumque harum architecto aliquam eius, porro perspiciatis. Perspiciatis eum, maxime laudantium, sed a iure id odio nobis facere quidem soluta.</p>
         </main>
         <footer>
             <p> copyright 2024</p>
